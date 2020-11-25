ef4907d4510570ff22c620cbfaff2c59bfaa0cb8
Algotron <tom.kleyntsens@hotmail.com>
2020-11-25 17:26:02 +0100

    branch test

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,27 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mshell.h                                           :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkleynts <tkleynts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/19 11:15:26 by tkleynts          #+#    #+#             */
-/*   Updated: 2020/11/25 17:25:13 by tkleynts         ###   ########.fr       */
+/*   Created: 2020/11/25 17:21:46 by tkleynts          #+#    #+#             */
+/*   Updated: 2020/11/25 17:22:15 by tkleynts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MSHELL_H
-# define MSHELL_H
-
-# include "../libft/libft.h"
-
-typedef struct s_cmd
+void	parser(char **tokens)
 {
-	char			**argv;
-	int				pipe;
-	int				redir;
-	struct s_cmd	*nxt;
-}	t_cmd;
 
-
-#endif
+}
