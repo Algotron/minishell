2d72a06d6079520e2abea35afac6181b4defbaa4
Tom Kleyntsens <tkleynts@shi-r10-p1.s19.be>
2020-11-27 16:00:34 +0100

    created lst handeing functions

--- includes/mshell.h
+++ includes/mshell.h
@@ -6,7 +6,7 @@
 /*   By: tkleynts <tkleynts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 11:15:26 by tkleynts          #+#    #+#             */
-/*   Updated: 2020/11/25 17:30:45 by tkleynts         ###   ########.fr       */
+/*   Updated: 2020/11/27 13:07:48 by tkleynts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,17 @@
 
 # include "../libft/libft.h"
 
+# define ALLOC_ERROR -1
+# define OPEN_ERROR -2
+# define READ_ERROR -3
+# define WRITE_ERROR -4
+
 typedef struct s_cmd
 {
 	char			**argv;
 	int				pipe;
 	int				redir;
-	struct s_cmd	*nxt;
+	struct s_cmd	*next;
 }	t_cmd;
 
 typedef struct
@@ -28,5 +33,7 @@ typedef struct
 	struct s_cmd	*cmd_list;
 }	t_mdata;
 
+
+void	lstadd_back(t_cmd **alst, t_cmd *new);
 
 #endif
--- srcs/lst.c
+++ srcs/lst.c
@@ -1,39 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
+/*   lst.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkleynts <tkleynts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/25 17:21:46 by tkleynts          #+#    #+#             */
-/*   Updated: 2020/11/27 15:33:12 by tkleynts         ###   ########.fr       */
+/*   Created: 2020/11/27 12:26:55 by tkleynts          #+#    #+#             */
+/*   Updated: 2020/11/27 13:11:31 by tkleynts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mshell.h"
 
-t_mdata	*get_mdata()
+int		new_elem(t_cmd **cmd_lst)
 {
-	static t_mdata	mdata;
-	return (&mdata);
+	t_cmd *tmp;
+
+	if (!(tmp = (t_cmd*)malloc(sizeof(t_cmd))))
+		return (ALLOC_ERROR);
+	lstadd_back(cmd_lst, tmp);
 }
 
-void	parser(char **tokens)
+t_cmd	*lstlast(t_cmd *lst)
 {
-	int		j;
-	t_mdata *mdata;
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
 
-	j = 0;
-	mdata = get_mdata();
-	while (tokens[j])
+void	lstadd_back(t_cmd **alst, t_cmd *new)
+{
+	if (!new || !alst)
+		return ;
+	if (!lstlast(*alst))
+		*alst = new;
+	else
 	{
-		if (*tokens[j] == ';')
-		{
-			if ((j - 1 >= 0 && *tokens[j - 1] == ';') || (*tokens[j + 1] == ';'))
-				return (-1); //plus de 1 ; d'affilee
-		}
-
-		
+		new->next = NULL;
+		lstlast(*alst)->next = new;
 	}
- 	
 }
